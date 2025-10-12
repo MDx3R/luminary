b866a342d7a76886625cb8e9548fdcde11cbcec8
MDx3R <nationmdxr@gmail.com>
2025-10-12 13:26:07 +0300

    feat: define structure and common and assistant domains

---
+++ src/luminary/assistant/domain/entity/assisnant.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+from typing import Self
+from uuid import UUID
+
+from common.domain.exceptions import InvariantViolationError
+
+
+@dataclass(frozen=True)
+class Instructions:
+    prompt: str
+
+    def __post_init__(self) -> None:
+        if not self.prompt.strip():
+            raise InvariantViolationError("Prompt cannot be empty")
+
+
+@dataclass
+class Assistant:
+    assistant_id: UUID
+    user_id: UUID
+    name: str
+    description: str
+    instructions: Instructions | None
+
+    def __post_init__(self) -> None:
+        if not self.name.strip():
+            raise InvariantViolationError("Assistant name cannot be empty")
+        if not self.description.strip():
+            raise InvariantViolationError("Assistant description cannot be empty")
+
+    def change_name(self, new_name: str) -> None:
+        self.name = new_name
+
+    def change_description(self, new_description: str) -> None:
+        self.description = new_description
+
+    def change_instructions(self, new_instructions: Instructions) -> None:
+        self.instructions = new_instructions
+
+    def remove_instructions(self) -> None:
+        self.instructions = None
+
+    @classmethod
+    def create(
+        cls,
+        assistant_id: UUID,
+        user_id: UUID,
+        name: str,
+        description: str,
+        instructions: Instructions | None,
+    ) -> Self:
+        return cls(assistant_id, user_id, name, description, instructions)

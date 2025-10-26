11e84f11c6829ab472f21d5ea00e806954acd49f
MDx3R <nationmdxr@gmail.com>
2025-10-26 14:01:55 +0300

    feat: remove auto chat create with folder

--- src/luminary/folder/application/usecases/command/create_folder_use_case.py
+++ src/luminary/folder/application/usecases/command/create_folder_use_case.py
@@ -1,12 +1,5 @@
-from uuid import UUID, uuid4
+from uuid import UUID
 
-from common.application.interfaces.transactions.unit_of_work import IUnitOfWork
-
-from luminary.chat.application.interfaces.repositories.chat_repository import (
-    IChatRepository,
-)
-from luminary.chat.domain.entity.chat import ChatSettings
-from luminary.chat.domain.interfaces.chat_factory import IChatFactory
 from luminary.folder.application.interfaces.repositories.folder_repository import (
     IFolderRepository,
 )
@@ -21,18 +14,10 @@ from luminary.folder.domain.interfaces.folder_factory import (
 
 class CreateFolderUseCase(ICreateFolderUseCase):
     def __init__(
-        self,
-        uow: IUnitOfWork,
-        folder_factory: IFolderFactory,
-        chat_factory: IChatFactory,
-        folder_repository: IFolderRepository,
-        chat_repository: IChatRepository,
+        self, folder_factory: IFolderFactory, folder_repository: IFolderRepository
     ) -> None:
-        self.uow = uow
         self.folder_factory = folder_factory
-        self.chat_factory = chat_factory
         self.folder_repository = folder_repository
-        self.chat_repository = chat_repository
 
     async def execute(self, command: CreateFolderCommand) -> UUID:
         folder = self.folder_factory.create(
@@ -42,17 +27,7 @@ class CreateFolderUseCase(ICreateFolderUseCase):
             command.model_id,
             command.assistant_id,
         )
-        chat = self.chat_factory.create(
-            folder.folder_id,
-            command.user_id,
-            name=None,
-            settings=ChatSettings(
-                uuid4(), "prompt", 10
-            ),  # TODO: Replace with assistant service call
-        )
 
-        async with self.uow:
-            await self.folder_repository.add(folder)
-            await self.chat_repository.add(chat)
+        await self.folder_repository.add(folder)
 
         return folder.folder_id

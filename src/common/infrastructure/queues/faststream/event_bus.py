c6175e122a7653eb70c711d012620a7237c07f2c
MDx3R <nationmdxr@gmail.com>
2025-11-30 19:25:19 +0300

    feat: add build route method

--- src/common/infrastructure/queues/faststream/event_bus.py
+++ src/common/infrastructure/queues/faststream/event_bus.py
@@ -1,9 +1,10 @@
 import asyncio
 from collections.abc import Iterable
 
+from common.application.interfaces.handlers.handler import EVENT, IEventHandler
 from common.application.interfaces.services.event_bus import IEventBus
 from common.domain.events.event import Event
-from faststream.rabbit import RabbitBroker
+from faststream.rabbit import RabbitBroker, RabbitRoute
 from pydantic.alias_generators import to_snake
 
 
@@ -14,13 +15,24 @@ class FastStreamRabbitMQEventBus(IEventBus):
 
     async def publish(self, event: Event) -> None:
         await self.broker.publish(  # pyright: ignore[reportUnknownMemberType]
-            message=event, queue=self.build_queue(event)
+            message=event, queue=self.build_queue(self.prefix, type(event))
         )
 
     async def publish_all(self, events: Iterable[Event]) -> None:
         await asyncio.gather(*[self.publish(e) for e in events])
 
-    def build_queue(self, event: Event) -> str:
+    @staticmethod
+    def build_queue(prefix: str, event: type[Event]) -> str:
         entity_type = to_snake(event.aggregate_type())
         event_type = to_snake(event.event_type())
-        return f"{self.prefix}.{entity_type}.{event_type}"
+        return f"{prefix}.{entity_type}.{event_type}"
+
+    @classmethod
+    def build_route(
+        cls, prefix: str, event_type: type[EVENT], handler: IEventHandler[EVENT]
+    ) -> RabbitRoute:
+        async def wrapper(event: EVENT) -> None:
+            await handler.handle(event)
+
+        wrapper.__annotations__["event"] = event_type
+        return RabbitRoute(wrapper, queue=cls.build_queue(prefix, event_type))

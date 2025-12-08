28c5e8f927da98a0455fb12833ce0db31db3c3a0
gepron1x <georgijpronuk@gmail.com>
2025-12-08 17:02:22 +0300

    refactor: test_folder_entity.py

--- tests/unit/folder/domain/test_folder_entity.py
+++ tests/unit/folder/domain/test_folder_entity.py
@@ -1,85 +1,109 @@
 from datetime import UTC, datetime
+from typing import Literal
 from uuid import uuid4
 
 import pytest
 from common.domain.exceptions import InvariantViolationError
 from common.domain.value_objects.datetime import DateTime
+from common.domain.value_objects.id import UserId
 
-from luminary.folder.domain.entity.folder import Folder, FolderInfo
+from luminary.assistant.domain.entity.assisnant import AssistantId
+from luminary.chat.domain.value_objects.chat_id import ChatId
+from luminary.folder.domain.entity.folder import Folder
+from luminary.folder.domain.value_objects.folder_id import FolderId
+from luminary.folder.domain.value_objects.folder_info import FolderInfo
+from luminary.source.domain.entity.source import SourceId
 
 
-class TestFolderEntity:
-    @pytest.fixture
-    def folder_data(self):
-        return {
-            'folder_id': uuid4(),
-            'user_id': uuid4(),
-            'name': "Test Folder",
-            'description': "Test Description",
-            'model_id': uuid4(),
-            'assistant_id': uuid4(),
-            'created_at': DateTime(datetime.now(UTC))
-        }
+class TestFolderInfo:
+    def test_create_success(self):
+        info = FolderInfo(name="Folder Info", description="Folder Description")
+        assert info.name == "Folder Info"
+        assert info.description == "Folder Description"
 
-    def test_create_folder_success(self, folder_data):
-        folder = Folder.create(**folder_data)
-        
-        assert folder.folder_id == folder_data['folder_id']
-        assert folder.user_id == folder_data['user_id']
-        assert folder.info.name == folder_data['name']
-        assert folder.info.description == folder_data['description']
-        assert folder.model_id == folder_data['model_id']
-        assert folder.assistant_id == folder_data['assistant_id']
-
-    def test_folder_info_empty_name_raises_error(self):
+    @pytest.mark.parametrize("name", ["", "   "])
+    def test_invalid_name_raises(self, name: Literal[""] | Literal["   "]):
         with pytest.raises(InvariantViolationError):
-            FolderInfo(name="", description="Test")
+            FolderInfo(name=name, description="Folder Description")
 
-    def test_change_name_success(self, folder_data):
-        folder = Folder.create(**folder_data)
+
+class TestFolder:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.folder_id = FolderId(uuid4())
+        self.owner_id = UserId(uuid4())
+
+        self.name = "Folder Name"
+        self.description = "Folder Description"
+
+        self.assistant_id = AssistantId(uuid4())
+        self.created_at = DateTime(datetime.now(UTC))
+
+        self.folder = Folder(
+            id=self.folder_id,
+            owner_id=self.owner_id,
+            info=FolderInfo(name=self.name, description=self.description),
+            assistant_id=self.assistant_id,
+            created_at=self.created_at,
+        )
+
+    def test_create_folder_success(self):
+        folder = Folder.create(
+            id=self.folder_id,
+            owner_id=self.owner_id,
+            name=self.name,
+            description=self.description,
+            assistant_id=self.assistant_id,
+            created_at=self.created_at,
+        )
+
+        assert folder == self.folder
+
+    def test_change_name_success(self):
         new_name = "Updated Name"
-        
-        folder.change_name(new_name)
-        
-        assert folder.info.name == new_name
-        assert folder.info.description == folder_data['description']
+        self.folder.change_name(new_name)
+        assert self.folder.info.name == new_name
 
-    def test_change_description_success(self, folder_data):
-        folder = Folder.create(**folder_data)
+    @pytest.mark.parametrize("name", ["", "   "])
+    def test_change_name_invalid(self, name: Literal[""] | Literal["   "]):
+        with pytest.raises(InvariantViolationError):
+            self.folder.change_name(name)
+
+    def test_change_description_success(self):
         new_description = "Updated Description"
-        
-        folder.change_description(new_description)
-        
-        assert folder.info.description == new_description
-        assert folder.info.name == folder_data['name']
+        self.folder.change_description(new_description)
+        assert self.folder.info.description == new_description
 
-    def test_add_and_remove_chat(self, folder_data):
-        folder = Folder.create(**folder_data)
-        chat_id = uuid4()
-        
-        folder.add_chat(chat_id)
-        assert folder.has_chat(chat_id)
-        
-        folder.remove_chat(chat_id)
-        assert not folder.has_chat(chat_id)
+    def test_add_and_remove_chat(self):
+        chat_id = ChatId(uuid4())
 
-    def test_add_and_remove_source(self, folder_data):
-        folder = Folder.create(**folder_data)
-        source_id = uuid4()
-        
-        folder.add_source(source_id)
-        assert folder.has_source(source_id)
-        
-        folder.remove_source(source_id)
-        assert not folder.has_source(source_id)
+        self.folder.add_chat(chat_id)
+        assert self.folder.has_chat(chat_id)
 
-    def test_change_model_and_assistant(self, folder_data):
-        folder = Folder.create(**folder_data)
-        new_model_id = uuid4()
-        new_assistant_id = uuid4()
-        
-        folder.change_model(new_model_id)
-        folder.change_assistant(new_assistant_id)
-        
-        assert folder.model_id == new_model_id
-        assert folder.assistant_id == new_assistant_id
+        self.folder.remove_chat(chat_id)
+        assert not self.folder.has_chat(chat_id)
+
+    def test_add_and_remove_source(self):
+        source_id = SourceId(uuid4())
+
+        self.folder.add_source(source_id)
+        assert self.folder.has_source(source_id)
+
+        self.folder.remove_source(source_id)
+        assert not self.folder.has_source(source_id)
+
+    def test_change_assistant(self):
+        new_assistant_id = AssistantId(uuid4())
+        self.folder.change_assistant(new_assistant_id)
+        assert self.folder.assistant_id == new_assistant_id
+
+    def test_with_none_assistant_id(self):
+        folder = Folder.create(
+            id=self.folder_id,
+            owner_id=self.owner_id,
+            name=self.name,
+            description=self.description,
+            assistant_id=None,
+            created_at=self.created_at,
+        )
+        assert folder.assistant_id is None

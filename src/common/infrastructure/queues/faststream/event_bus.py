4dbccf47bb2e3ce9c7a7d4617c8a3bf2b9ec5928
MDx3R <nationmdxr@gmail.com>
2025-12-08 05:09:27 +0300

    test: event bus

--- src/common/infrastructure/queues/faststream/event_bus.py
+++ src/common/infrastructure/queues/faststream/event_bus.py
@@ -4,22 +4,30 @@ from collections.abc import Iterable
 from common.application.interfaces.handlers.handler import EVENT, IEventHandler
 from common.application.interfaces.services.event_bus import IEventBus
 from common.domain.events.event import Event
+from common.domain.interfaces.uuid_generator import IUUIDGenerator
 from faststream.rabbit import RabbitBroker, RabbitRoute
 from pydantic.alias_generators import to_snake
 
 
 class FastStreamRabbitMQEventBus(IEventBus):
-    def __init__(self, broker: RabbitBroker, prefix: str = "") -> None:
+    def __init__(
+        self, broker: RabbitBroker, uuid_generator: IUUIDGenerator, prefix: str = ""
+    ) -> None:
         self.broker = broker
         self.prefix = prefix
+        self.uuid_generator = uuid_generator
 
     async def publish(self, event: Event) -> None:
+        event = self.init_event(event)
         await self.broker.publish(  # pyright: ignore[reportUnknownMemberType]
             message=event, queue=self.build_queue(self.prefix, type(event))
         )
 
     async def publish_all(self, events: Iterable[Event]) -> None:
         await asyncio.gather(*[self.publish(e) for e in events])
+
+    def init_event(self, event: EVENT) -> EVENT:
+        return event.set_event_id(self.uuid_generator.create())
 
     @staticmethod
     def build_queue(prefix: str, event: type[Event]) -> str:
--- src/luminary/folder/domain/events/events.py
+++ src/luminary/folder/domain/events/events.py
@@ -1,7 +1,7 @@
+from dataclasses import dataclass
 from uuid import UUID
 
 from common.domain.events.domain_event import DomainEvent
-from pydantic.dataclasses import dataclass
 
 
 @dataclass(frozen=True)

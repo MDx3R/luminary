2610772e4b0e193016b2f539976e8f0d96881480
gepron1x <georgijpronuk@gmail.com>
2025-12-08 17:28:32 +0300

    refactor: test_folder_factory

--- tests/unit/folder/domain/test_folder_factory.py
+++ tests/unit/folder/domain/test_folder_factory.py
@@ -1,39 +1,52 @@
-from unittest.mock import Mock
+from datetime import UTC, datetime
 from uuid import uuid4
 
 import pytest
+from common.domain.value_objects.datetime import DateTime
+from common.domain.value_objects.id import UserId
+from tests.unit.utils import MockClock, MockUUIDGenerator
 
+from luminary.assistant.domain.entity.assisnant import AssistantId
 from luminary.folder.domain.entity.folder import Folder
 from luminary.folder.domain.factories.folder_factory import FolderFactory
+from luminary.folder.domain.value_objects.folder_id import FolderId
 
 
 class TestFolderFactory:
-    @pytest.fixture
-    def factory(self):
-        clock = Mock()
-        clock.now.return_value = Mock()
-        uuid_generator = Mock()
-        uuid_generator.create.return_value = uuid4()
-        
-        return FolderFactory(clock, uuid_generator)
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.name = "Folder Name"
+        self.description = "Folder Description"
+        self.folder_id = FolderId(uuid4())
+        self.owner_id = UserId(uuid4())
+        self.assistant_id = AssistantId(uuid4())
+        self.created_at = DateTime(datetime.now(UTC))
 
-    def test_create_folder_success(self, factory):
-        name = "Test Folder"
-        description = "Test Description"
-        user_id = uuid4()
-        model_id = uuid4()
-        assistant_id = uuid4()
-        
-        folder = factory.create(name, description, user_id, model_id, assistant_id)
-        
+        self.factory = FolderFactory(
+            clock=MockClock(self.created_at),
+            uuid_generator=MockUUIDGenerator(self.folder_id.value),
+        )
+
+    def test_create_folder_success(self):
+        folder = self.factory.create(
+            name=self.name,
+            description=self.description,
+            user_id=self.owner_id,
+            assistant_id=self.assistant_id,
+        )
+
         assert isinstance(folder, Folder)
-        assert folder.info.name == name
-        assert folder.info.description == description
-        assert folder.user_id == user_id
-        assert folder.model_id == model_id
-        assert folder.assistant_id == assistant_id
+        assert folder.info.name == self.name
+        assert folder.info.description == self.description
+        assert folder.owner_id == self.owner_id
+        assert folder.assistant_id == self.assistant_id
 
-    def test_create_folder_with_none_description(self, factory):
-        folder = factory.create("Test", None, uuid4(), uuid4(), uuid4())
-        
+    def test_create_folder_with_none_description(self):
+        folder = self.factory.create(
+            name=self.name,
+            description=None,
+            user_id=self.owner_id,
+            assistant_id=self.assistant_id,
+        )
+
         assert folder.info.description is None
